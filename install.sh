d6772c411547928da7a88cb171c96040daf7e3d0
Maijorpain <66681553+Maijorpain@users.noreply.github.com>
2024-07-27 19:53:15 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ./fingerprint/
 chmod +x install.sh
-./install.sh
+sudo /bin/sh install.sh fingerprint 
 sudo dnf install -y podman
 sudo podman build -t fingerprint -f Dockerfile --build-arg UNAME=$USER
 DEV_PATH=$(lsusb -d 10a5:9201 | awk '{print $2"/"$4}' | tr -d :)
