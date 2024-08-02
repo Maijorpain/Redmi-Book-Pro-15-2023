f3a37b5236526b1dd3b279ceea8414504c825243
Maijorpain <66681553+Maijorpain@users.noreply.github.com>
2024-08-02 14:35:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo /bin/sh install.sh fingerprint
 sudo dnf install -y podman
 sudo podman build -t fingerprint -f Dockerfile --build-arg UNAME=$USER
 DEV_PATH=$(lsusb -d 10a5:9201 | awk '{print $2"/"$4}' | tr -d :)
-sudo cp ../fingerprint.service //etc/systemd/system/fingerprint.service
+sudo cp ../fingerprint.service /etc/systemd/system/fingerprint.service
 sudo systemctl enable fingerprint.service
 sudo systemctl start fingerprint.service
